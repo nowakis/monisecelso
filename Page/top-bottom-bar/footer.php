af0697501fae652f752ed5425e7b32d5b1776ba4
Fabio Nowaki <fabio.nowaki@gmail.com>
2018-04-23 22:08:12 -0300

    Novas fotos

--- Page/top-bottom-bar/footer.php
+++ Page/top-bottom-bar/footer.php
@@ -2,7 +2,7 @@
         
         <!-- FOOTER SECTION -->
             <footer>
-            	<div class="image-divider fixed-height" style="background-image:url(images/normal/foto-94.jpg);" data-stellar-background-ratio="0.5" >
+            	<div class="image-divider fixed-height" style="background-image:url(images/normal/foto-94-resized.jpg);" data-stellar-background-ratio="0.5" >
                 
                 	<div class="divider-overlay"></div>
                     
